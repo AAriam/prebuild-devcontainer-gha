fa427d894e2da57a9de73a8c3d618c718b3305be
Sebastian Steins <592313+sebst@users.noreply.github.com>
2025-03-03 11:46:16 +0100

    Add devcontainer config

---
+++ src/main.py
@@ -0,0 +1,15 @@
+def is_python_3_12():
+    try:
+        s = f"{f"{f"{f"{f"{f"{1+1}"}"}"}"}"}"
+    except:
+        return False
+    return True
+
+def main():
+    if is_python_3_12():
+        print("Python 3.12")
+    else:
+        print("Not Python 3.12")
+
+if __name__ == "__main__":
+    main()
